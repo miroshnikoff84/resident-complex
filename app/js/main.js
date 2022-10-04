78c599ca1e5344004a4a6338235a3fc56fb3af86
miros <miroshnikoff84@mail.ru>
2022-10-04 20:54:00 +0300

    the layout is finished

--- app/js/main.js
+++ app/js/main.js
@@ -1,8 +1,22 @@
 $(function () {
-    $('.menu__list-item').click(function() {
-        var id = $(this).attr('data-tab');
-   
-   $('.menu__list-item.menu__list-item--active').removeClass('menu__list-item--active');
-   $(this).addClass('menu__list-item--active'); 
-   })
+    $('.menu__list-item').on('click', function () {
+
+        $('.menu__list-item.menu__list-item--active').removeClass('menu__list-item--active');
+        $(this).addClass('menu__list-item--active');
+    })
+
+    $('#overlay-show').on('click', function () {
+        $('.overlay').removeClass('overlay-hidden');
+        $('.menu__list-item.menu__list-item--active').removeClass('menu__list-item--active');
+
+    })
+    
+    $('.modal__close').on('click', function () {
+        $('.overlay').addClass('overlay-hidden');
+        $('.menu__list-item.menu__list-item--active').removeClass('menu__list-item--active');
+        $('#first-item').addClass('menu__list-item--active');
+    })
+
+
+
 })
